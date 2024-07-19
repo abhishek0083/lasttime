96cb75a4e06e1e06ca56663ec231b0b0e1d60b4c
abhishek0083 <146963477+abhishek0083@users.noreply.github.com>
2024-07-19 18:05:18 +0530

    Update add.java

--- add.java
+++ add.java
@@ -2,7 +2,9 @@ public class Solution{
 	public static void main(String[] args){
 		Scanner sc = new Scanner(System.in);
 		int n = sc.nextInt();
-		System.out.println(n);
+		for(int i=1;i<n;i++){
+		System.out.println(i);
+		}
 	}
 }
 
